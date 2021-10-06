562d41e7b774bfe9171ebe2b851488c9599c101d
Yacouba Keita <theyacoubakeita@gmail.com>
2021-10-06 12:13:47 +0000

    Add create method

--- index.php
+++ index.php
@@ -1,16 +1,3 @@
 <?php
 
 require 'vendor/autoload.php';
-
-use App\config\Database;
-use App\Controllers\Controller;
-use App\Models\Model;
-use App\Models\Post;
-
-$db = new Database();
-
-$db->pdo();
-
-$post = new Post;
-
-var_dump($post->delete(4));
--- src/Models/Post.php
+++ src/Models/Post.php
@@ -2,7 +2,45 @@
 
 namespace App\Models;
 
+use App\config\Database;
+
 class Post extends Model
 {
     protected string $table = "posts";
+
+    protected string $title;
+    protected string $description;
+
+    public function getTitle()
+    {
+        return $this->title;
+    }
+
+    public function setTitle(string $title)
+    {
+        $this->title = $title;
+    }
+
+    public function getDescription()
+    {
+        return $this->description;
+    }
+
+    public function setDescription(string $description)
+    {
+        $this->description = $description;
+    }
+
+
+    public function create()
+    {
+        $database = new Database();
+        $pdo = $database->pdo();
+        
+        $q = $pdo->prepare("INSERT INTO $this->table(title, description) VALUES (:title, :description)");
+        $q->execute([
+            'title' => $this->getTitle(),
+            'description' => $this->getDescription()
+        ]);
+    }
 }
